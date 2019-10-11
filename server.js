6462ad87e8660f5bcc677a567a1141e8ef4ce9de
Sue Look <suelook1@gmail.com>
2019-10-11 13:53:06 -0400

    initialized server.js

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 //include the packages (remember to run "npm install <package>")
 var express = require("express");
 var path = require("path");
+
+//include js files
+const friends = require ("./app/data/friends");
+const htmlRoutes = require ("./app/routing/htmlRoutes");
+const apiRoutes = require ("./app/routing/apiRoutes");
+
+//set up the express app
+var app = express();
+var PORT = 3000; 
+
+// set up the express app to handle data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
