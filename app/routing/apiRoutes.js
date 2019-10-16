bbf6fe5afb203737ad8f9f352402143ddc53b2b9
Sue Look <suelook1@gmail.com>
2019-10-16 09:15:24 -0400

    created logic for comparing new friend compatability

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,9 +13,36 @@ module.exports = function (app) {
     //create new friends in JSON format
     app.post("/api/friends", function (req, res) {
         var newFriend = req.body;
-        newFriend.name = newFriend.name.replace(/\s+/g, "").toUpperCase();
+
+        // point at the scores for this new friend
+        var friendScores = newFriend.scores;
+
+        // initialize a few variables
+        var bffName = '';
+        var bffPhoto = '';
+
+        // set up a "high score", lower is better
+        var bffScore = 50;
+
+        // loop through the other friends
+        for (var i = 0; i < friends.length; i++) {
+
+            // calculate the difference between this friend and all the others
+            var thisFriendScore = 0;
+            for (var question = 0; question < friendScores.length; question++) {
+                thisFriendScore = thisFriendScore + Math.abs(friends[i].scores[question] - friendScores[question]);
+            };
+
+            // If this one is lower, that's the new bff
+            if (thisFriendScore < bffScore) {
+                bffScore = thisFriendScore;
+                bffName = friends[i].name;
+                bffPhoto = friends[i].photo;
+            };
+        };
+
+        // Add new user (friends.push) and return
         friends.push(newFriend);
-        console.log(JSON.stringify(friends));
-        res.json(newFriend);
+        res.json({ bffName: bffName, bffPhoto: bffPhoto });
     });
 };
