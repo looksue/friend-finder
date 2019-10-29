2109295c9741029615822d775786d305e0dccc25
Sue Look <suelook1@gmail.com>
2019-10-29 18:41:01 -0400

    Added friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,14 @@
 //Friends data
 
 var friends = [
-    {
-        "name":"Ahmed",
-        "photo":"https://cdn.pixabay.com/photo/2015/08/02/21/49/caricature-871836_960_720.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
-      }
-   ]
+    { "name": "Ahmed", "photo": "https://cdn.pixabay.com/photo/2015/08/02/21/49/caricature-871836_960_720.jpg", "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1] },
+    { "name": "Pete", "photo": "https://images.unsplash.com/photo-1521714161819-15534968fc5f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80", "scores": ["4", "3", "3", "1", "5", "2", "4", "3", "2", "2"] },
+    { "name": "Sookie", "photo": "https://upload.wikimedia.org/wikipedia/en/8/8a/Sookie_%28TB%29.jpg", "scores": ["5", "5", "5", "5", "5", "1", "5", "3", "3", "2"] },
+    { "name": "Thor", "photo": "https://www.tokkoro.com/picsup/2632102-thor-ragnarok-4k-latest-wallpaper-free-download.jpg", "scores": ["5", "4", "2", "2", "1", "5", "5", "5", "5", "5"] },
+    { "name": "Joanna", "photo": "https://dtpmhvbsmffsz.cloudfront.net/posts/2017/06/22/594be0b3f0137d5e860065d4/m_594be0c17fab3a1ddf006746.jpg", "scores": ["3", "5", "4", "4", "5", "3", "1", "3", "5", "5"] },
+    { "name": "Jen", "photo": "https://wallpaperaccess.com/full/1090950.jpg", "scores": ["4", "3", "4", "5", "3", "2", "2", "4", "3", "1"] },
+    { "name": "Stephanie", "photo": "https://upload.wikimedia.org/wikipedia/commons/1/11/Stephanie_Wilson_in_2008.jpg", "scores": ["4", "2", "3", "2", "4", "3", "3", "3", "3", "4"] }
+]
 
-   // export the friends array for other programs
-   module.exports = friends;
+// export the friends array for other programs
+module.exports = friends;
